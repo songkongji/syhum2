1d8d81ef4e251c3fe37f95209334ffc6094677b4
songE <seungmin576@gmail.com>
2023-05-22 17:26:10 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,17 @@
+$(function () {
+    var img = $('img'),
+        imgshow = $('#imgshow'),
+        overly = $('#over'),
+        close = $('#close');
+        img.on('click', function () {
+        // overly 영역 보임
+        overly.fadeIn();
+
+        imgshow.attr('src', $(this).attr('src'));
+
+        //close를 클릭하면 overly 영역 숨김
+        close.on('click', function() {
+          overly.hide();
+        });  
+        });
+      });
